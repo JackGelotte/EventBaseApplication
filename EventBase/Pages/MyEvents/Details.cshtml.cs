3ba7e1c4f2ecd40e7a8c1f40f67d00ab41a0ee8c
Calphonar <Richardzejak@hotmail.com>
2021-03-31 14:08:50 +0200

    Added MyEvents Window and leave event function

---
+++ EventBase/Pages/MyEvents/Details.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EventBase.Data;
+using EventBase.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventBase.Pages.MyEvents
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly EventBase.Data.EventBaseContext _context;
+        private readonly UserManager<MyUser> _userManager;
+
+        public DetailsModel(EventBase.Data.EventBaseContext context, UserManager<MyUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.ID == id);
+
+            var userId = _userManager.GetUserId(User);
+
+            var MyUser = await _context.MyUsers.Where(u => u.Id == userId).Include(u => u.JoinedEvents).FirstOrDefaultAsync();
+
+            if (MyUser == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                MyUser.JoinedEvents.Remove(Event);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            return Page();
+        }
+    }
+}
---
+++ EventBase/Pages/MyEvents/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EventBase.Data;
+using EventBase.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventBase.Pages.MyEvents
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EventBase.Data.EventBaseContext _context;
+        private readonly UserManager<MyUser> _userManager;
+
+        public IndexModel(EventBase.Data.EventBaseContext context, UserManager<MyUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public ICollection<Event> Event { get;set; }
+        public MyUser MyUser { get; set; }
+        
+        public async Task OnGetAsync()
+        {
+
+            var userId = _userManager.GetUserId(User);
+
+            var MyUser = await _context.MyUsers.Where(u => u.Id == userId).Include(u => u.JoinedEvents).FirstOrDefaultAsync();
+            Event = MyUser.JoinedEvents;
+        }
+    }
+}
