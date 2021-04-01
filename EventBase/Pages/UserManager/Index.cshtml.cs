5c9c753afbf0c462c4e06ab9cb080ca381bd4f58
Calphonar <Richardzejak@hotmail.com>
2021-04-01 14:13:31 +0200

    Added manageUsers page

---
+++ EventBase/Pages/UserManager/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EventBase.Data;
+using EventBase.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EventBase.Pages.UserManager
+{   [Authorize(Policy = "RequireAdminRole")]
+    public class IndexModel : PageModel
+    {
+        private readonly EventBase.Data.EventBaseContext _context;
+
+        public IndexModel(EventBase.Data.EventBaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<MyUser> MyUsers { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            MyUsers = await _context.MyUsers.ToListAsync();
+        }
+    }
+}
